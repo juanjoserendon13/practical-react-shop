ab91b10707ec5dc8b80b3760d2fe6d0a931cb538
Juan Jose Rendon <juanjoserendon13@outlook.com>
2021-12-14 00:00:36 -0500

    create specific environments for webpack

--- webpack.common.js
+++ webpack.common.js
@@ -4,8 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    clean: true
   },
   resolve: {
     extensions: ['.js','.jsx']

960a719006c0e963deea4ebeb517e2423075c9d7
Juan Jose Rendon <juanjoserendon13@outlook.com>
2022-01-10 17:29:32 -0500

    add logic to fetch products data

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ProductItem from '@components/ProductItem';
+import useGetProducts from '../hooks/useGetProducts';
 import '@styles/ProductList.scss';
 
+const API =  'http://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+	
+	const products = useGetProducts(API);
+	
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem key={product.id} product={product} />
+				))}
 			</div>
+			{console.log(products)}
 		</section>
 	);
 }
